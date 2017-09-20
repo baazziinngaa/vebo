f487ed2f227e375333d884683586c18b0d0c49e7
baazziinngaa <mateusz11116@gmail.com>
2017-09-20 19:01:11 +0200

    Add project files.

---
+++ vebo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vebo.Startup))]
+namespace vebo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
